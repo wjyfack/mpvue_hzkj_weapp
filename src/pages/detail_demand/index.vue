76c3046095197e8b2649ea271ba1e4778f910393
wjyfack <wjyfack@qq.com>
2018-11-29 16:46:16 +0800

    添加需求详情

---
+++ src/pages/detail_demand/index.vue
@@ -0,0 +1,176 @@
+<template>
+<div class="demand">
+    <div class="header van-hairline--bottom">
+        <div class="title">皮卡刻字机脱机状态小车走纸轴不动轴不动轴皮卡刻字机脱机状态小车走纸轴不动轴不动轴</div>
+        <div class="mini">
+            <div>发布于：2018-11-14</div>
+            <div class="eye"><img src="../../../static/imgs/eye.png" alt="" class="eye-img">9999+</div>
+        </div>
+    </div>
+    <div class="content">
+        <div>皮卡刻字机1200脱机状态 小车不动 走纸轴不动 风扇也不转 摁任何键子机器显示器上都有数值变化 就是小车不动 走纸轴叶不动 联机状态也一样 用刻绘大师软件输出 电脑读条走一点就不走了 刻字机显示器就会显示ERROR!+X Limit 我买了电源 驱动和主板 都换了 还是之前的状况 一点也没变化 求师傅上门检测维修</div>
+        <div class="cont-img">
+            <img src="http://placehold.it/100x100" alt="" class="cont-img-item">
+            <img src="http://placehold.it/100x100" alt="" class="cont-img-item">
+            <img src="http://placehold.it/100x100" alt="" class="cont-img-item">
+        </div>
+    </div>
+    <div class="good">
+        <div class="good-item">
+            <div class="hd">目标预算</div>
+            <div class="cont">1500.00元</div>
+        </div>
+        <div class="good-item">
+            <div class="hd">期望天数</div>
+            <div class="cont">7天</div>
+        </div>
+        <div class="good-item">
+            <div class="hd">需求者</div>
+            <div class="cont">吴***</div>
+        </div>
+        <div class="good-item">
+            <div class="hd">联系方式</div>
+            <div class="cont">189****6621</div>
+        </div>
+    </div>
+    <div class="bao">
+        <div class="hd">报价记录</div>
+        <div class="info">
+            <div class="info-item"><div class="name">林**</div> <div>2018-11-26</div></div>
+            <div class="info-item"><div>联系方式：158***333</div></div>
+            <div class="info-item"><div>预报单价：***</div></div>
+            <div class="info-item"><div>所需时间：***</div> 
+            <div class="opt">
+                <img src="../../../static/imgs/rp_gou.png" alt="" class="img">
+                <img src="../../../static/imgs/edit.png" alt="" class="img">
+            </div></div>
+        </div>
+    </div>
+    <div class="baojia">报价</div>
+    <div class="tips">
+        <div class="tips-item">1、以上所展示的信息由用户自行发布，内容的真实性、准确性和合法性由发布此信息的发布方负责。传动先生对此不承担任何保证责任。</div>
+        <div class="tips-item">2、我们建议您在跟该用户发生任何交易之前，务必先确认其资质，若是涉及金额过大需先签署正规合同。请谨慎交易，谨防欺诈行为。</div>
+    </div>
+</div>
+</template>
+<script>
+
+export default {
+    data() {
+    }
+    ,methods: {
+    }
+}
+</script>
+<style lang="less" scoped>
+.demand {
+    background: #F9F9F9;
+    .header {
+        margin-top: 5px;
+        padding: 10px 20px;
+        background: #ffffff;
+        .title {
+            font-size: 16px;
+            font-weight: bold;
+            color:#000;
+        }
+        .mini {
+            display: flex;
+            justify-content: space-between;
+            padding-top: 5px;
+            color:#8E8E8E;
+            font-size: 8px;
+            .eye {
+                .eye-img {
+                    width: 11px;
+                    height: 7px;
+                    margin-right: 5px;
+                }
+            }
+        }
+    }
+    .content {
+        color:#8E8E8E;
+        font-size: 12px;
+        background: #ffffff;
+        padding: 10px 20px;
+        margin-bottom: 5px;
+        .cont-img {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: flex-start;
+            margin-top: 5px;
+            &-item {
+                width: 76px;
+                height: 76px;
+                border-radius: 3px;
+                margin-right: 10px;
+            }
+        }
+    }
+    .good {
+        display: flex;
+        justify-content: space-around;
+        padding: 10px 20px;
+        margin-bottom: 5px;
+        background: #ffffff;
+        .good-item {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            font-size: 10px;
+            .hd {background: #5887F9;color:#ffffff;padding: 0 5px;border-radius: 8px;}
+            .cont {color:#8E8E8E;padding-top: 5px;}
+        }
+    }
+    .bao {
+        position: relative;
+        background: #ffffff;
+        padding: 10px 20px;
+        .hd {
+            border-left: 4px solid #5887F9;
+            color:#4C5264;
+            font-size: 14px;
+            padding-left: 5px;
+            line-height: 1;
+            margin-bottom: 10px;
+        }
+        .info {
+            margin: 10px 0;
+            border-radius: 5px;
+            background: #F6F6F6;
+            padding: 10px;
+            .info-item {
+                display: flex;
+                justify-content:space-between;
+                color:#8E8E8E;
+                font-size: 10px;
+                .name {color:#2E2E2E;}
+                .opt {
+                    display: flex;
+                    .img {width:15px;height: 15px;margin-left: 5px;}
+                }
+            }
+        }
+    }
+    .baojia {
+        margin: 10px 20px;
+        padding: 10px 20px;
+        text-align: center;
+        border-radius: 3px;
+        font-size: 14px;
+        background: #5887F9;
+        color:#ffffff;
+    }
+    .tips {
+        padding: 10px 20px 40px 10px;
+        color:#828282;
+        font-size: 10px;
+        .tips-item {
+            margin-bottom: 5px;
+        }
+    }
+}
+</style>
+
